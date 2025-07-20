0a9097c8fe6cbce6c69411986795fb1ffc47f45f
Andrea Ruvalcaba <ferchiz.peque@gmail.com>
2025-07-19 23:10:41 -0500

    Primer coomit

---
+++ js/tailwind-config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          green: '#D9f5d5',  // verde oscuro
+          blue: '#aedff7',    // turquesa / verde brillante
+          pink: '#f7c5cc',
+          yellow: '#fff3b0',
+          lila: '#E6e0f8',
+          gray: '#F8F8F8'
+        }
+      }
+    }
+  }
+}
